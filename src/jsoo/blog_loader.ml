b9dd35d904534d051f1dbdf49037c16a820ac1ea
Emile Trotignon <emile@tarides.com>
2023-10-26 15:08:27 +0200

    blog_loader loads list of article

--- src/jsoo/blog_loader.ml
+++ src/jsoo/blog_loader.ml
@@ -1,51 +1,65 @@
-(*
-(async () => {
-  const response = await fetch('https://api.github.com/repos/:user/:repo/contents/');
-  const data = await response.json();
-  let htmlString = '<ul>';
-  
-  for (let file of data) {
-    htmlString += `<li><a href="${file.path}">${file.name}</a></li>`;
-  }
-
-  htmlString += '</ul>';
-  document.getElementsByTagName('body')[0].innerHTML = htmlString;
-})()   
-*)
-
 open Brr
 open Fut.Syntax
+open Printf
 
-let don't_wait_for f = 
-  Fut.await f (fun () -> ())
+let don't_wait_for f = Fut.await f (fun () -> ())
 
-let ( .%{} ) obj prop = Jv.get obj prop 
+let ( .%{} ) obj prop = Jv.get obj prop
+
+let ( !% ) = Jstr.of_string
+
+let name_no_suffix name =
+  assert (String.ends_with ~suffix:".md" name) ;
+  String.sub name 0 (String.length name - 3)
+
+let name_pretty name = name |> name_no_suffix |> String.split_on_char '_' |> String.concat " "
+
+let name_html name = name_no_suffix name ^ ".html"
+
+let article_nav_section articles =
+  let document = G.document in
+  let navbar = Document.(find_el_by_id document !%"sidebar" |> Option.get) in
+  let blog_section =
+    El.(
+      section
+        ~at:At.[id !%"blog_nav"]
+        (List.map
+           (fun ((article_name : string), _content) ->
+             p [a ~at:At.[href !%(sprintf "/blog/%s" (name_html article_name))] [txt !%(name_pretty article_name)]] )
+           articles ) )
+  in
+  El.append_children navbar [blog_section]
 
 let run () =
-  let+ result = 
-  let open Fut.Result_syntax in
-  let open Brr_io.Fetch in
-  let* response = 
-    Brr_io.Fetch.(
-      request 
-        (Request.v ~init:(Request.init ())
-        (Jstr.of_string "https://api.github.com/repos/EmileTrotignon/blog/contents/")))
-  in
-  let+ data = response |> Response.as_body |> Body.json in
-  let urls = Jv.(to_array (fun elt -> elt.%{"git_url"} |> to_string) data) in
-  let contents =
-    Array.map (fun md_url ->
+  let+ result =
+    let open Fut.Result_syntax in
+    let open Brr_io.Fetch in
+    let* response =
       Brr_io.Fetch.(
-      request 
-        (Request.v ~init:(Request.init ())
-        (Jstr.of_string md_url)))
-      
-      ) urls in
-  Console.log [urls]
-in
-match result with
-| Error _e -> ()
-| Ok () -> ()
+        request (Request.v ~init:(Request.init ()) !%"https://api.github.com/repos/EmileTrotignon/blog/contents/") )
+    in
+    let* data = response |> Response.as_body |> Body.json in
+    let urls = Jv.(to_array (fun elt -> (to_string elt.%{"name"}, to_string elt.%{"git_url"})) data) in
+    let open Fut.Syntax in
+    let names, urls = Array.split urls in
+    let* contents =
+      urls
+      |> Array.map (fun md_url -> Brr_io.Fetch.(request (Request.v ~init:(Request.init ()) (Jstr.of_string md_url))))
+      |> Array.to_list |> Fut.of_list
+      |> Fut.map (List.map Result.get_ok)
+    in
+    let+ contents =
+      contents
+      |> List.map (fun response -> response |> Response.as_body |> Body.json)
+      |> Fut.of_list
+      |> Fut.map (List.map Result.get_ok)
+    in
+    let contents = List.map (fun jv -> Base64.decode (Jv.to_jstr jv.%{"content"}) |> Result.get_ok) contents in
+    let articles = List.combine (Array.to_list names) contents in
+    article_nav_section articles ;
+    Console.log [data] ;
+    Result.ok @@ Console.log [contents]
+  in
+  match result with Error _e -> () | Ok () -> ()
 
-let () = 
-  don't_wait_for (run ())
+let () = don't_wait_for (run ())
